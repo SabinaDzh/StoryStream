4eb7d8a966feb47245c1c6cee1690edd2432b7e4
SabinaDzh <163910584+SabinaDzh@users.noreply.github.com>
2024-06-07 17:07:13 +0300

    Update models.py

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -82,7 +82,9 @@ class Follow(models.Model):
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=['user', 'following'], name='unique_follow'),
+            models.UniqueConstraint(
+                fields=['user', 'following'],
+                name='unique_follow'),
         ]
 
     def __str__(self):
